e6fd468f38eb32178f81413c6918f0424fd3b09e
Humberto Arndt <humberto.arndt@gmail.com>
2022-10-13 17:55:09 -0300

    Add both Mario projects.

---
+++ week_1/mario_more.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    int sig;
+    int dots;
+    int height;
+    int width;
+
+    i = 0;
+    width = 1;
+    sig = 0;
+    while (sig == 0)
+    {
+        height = get_int("Height: ");
+        if (height >= 1 && height <= 8)
+            sig = 1;
+    }
+    dots = height - 1;
+    while (height--)
+    {
+        while (dots-- != 0)
+            printf(" ");
+        while (i++ < width)
+            printf("#");
+        printf("  ");
+        i = 0;
+        while (i++ < width)
+            printf("#");
+        i = 0;
+        width++;
+        dots = height - 1;
+        printf("\n");
+    }
+    return (0);
+}
